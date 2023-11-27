998c1bad565a4a2a7ccc7ae43201db9db5ea69a4
hakshaysundar <hakshay.a.sundar@gmail.com>
2023-11-27 15:43:49 -0500

    Added Edge mapping dictionary. Added string representation. Set up a dummy Network.

--- util.py
+++ util.py
@@ -24,6 +24,9 @@ class Node:
     
     def add_neighbors(self, nodes):
         self.neighbors.extend(nodes)
+    
+    def to_string(self):
+        return "Name: " + str(self.name) + " Type: " + self.type.to_string() + " Neighbors: " + str(self.neighbors)
 
 """
     Creating an enum to track the type of location. 
@@ -37,13 +40,20 @@ class LocationType(Enum):
     DULL = "Dull"
     VIBRANT = "Vibrant"
 
+    def to_string(self):
+        return str(self.value)
+
 """
     Creating a class for Graph which will instantiate and set up the graph our hitch-hiker will navigate.
 """
+import random
 class Graph:
+    node_number_mapping = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4}
+    
     def __init__(self):
         # Creating the start node where our hitch-hiker resides.
         self.current_node = Node("A", LocationType.START)
+        self.edge_mapping = Edge(5)
 
         # Temporary Nodes to test the concept.
         B = Node("B", LocationType.VIBRANT)
@@ -51,17 +61,48 @@ class Graph:
         D = Node("D", LocationType.VIBRANT)
         E = Node("E", LocationType.DULL)
 
-        self.current_node.add_neighbors([B, C, D, E])
+        for node in [B, C, D, E]:
+            self.current_node.add_neighbor(node)
+            self.edge_mapping.add_edge(self.node_number_mapping[self.current_node.fetch_location_name()], self.node_number_mapping[node.fetch_location_name()], random.randint(1, 10))
     
     def fetch_current_node(self):
         return self.current_node
 
+    def fetch_edge_mapping(self):
+        return self.edge_mapping
+
+"""
+    Creating a class for Edge which will keep track of the cost of traveling from one node to another.
+    It is just a database of all the nodes and gives us the ability to fetch the cost of traveling from one node to another.
+    As of now, it is a simple cost. It must change in the future to incorporate the cost of traveling from one node to another with different properties.
+"""
+class Edge:
+    def __init__(self, size):
+        # Keeps track of the length of row or column in an nxn matrix.
+        self.size = size
+        self.edges = [[-float("inf") for i in range(size)] for j in range(size)]
+    
+    def add_edge(self, node1, node2, cost):
+        self.edges[node1][node2] = cost
+        ### As of now, we are maintaining a symmetric matrix. We can change this in the future.
+        self.edges[node2][node1] = cost
+    
+    def fetch_cost(self, node1, node2):
+        return self.edges[node1][node2]
+    
+    def to_string(self):
+        return str(self.edges)
+
 
 def main():
     graph = Graph()
-    print(graph.fetch_current_node().fetch_location_name())
-    print(graph.fetch_current_node().fetch_location_type())
-    print(graph.fetch_current_node().fetch_neighbors())
+    print("Name: " + str(graph.fetch_current_node().fetch_location_name()))
+    print("Type: " + str(graph.fetch_current_node().fetch_location_type()))
+    print("Neighbors: ")
+    for node in graph.fetch_current_node().fetch_neighbors():
+        print(node.to_string())
+    print("Edge Mapping:")
+    print(graph.fetch_edge_mapping().to_string())
 
 if __name__ == "__main__":
     main()
